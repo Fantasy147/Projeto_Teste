f4d09596212d188aba50abc0d7520b68aaebe81b
Fantasy147 <fantasylife147@gmail.com>
2026-05-27 02:08:16 +0100

    Update and rename mainScript.js to script.js

---
+++ script.js
@@ -0,0 +1,27 @@
+
+
+script.js
+Javascript// Função para adicionar número na tela
+function pressKey(num) {
+    const screen = document.getElementById("screen");
+    if (screen.innerText === "Digite...") {
+        screen.innerText = num;
+    } else {
+        screen.innerText += num;
+    }
+}
+
+// Função para limpar a tela
+function clearScreen() {
+    document.getElementById("screen").innerText = "Digite...";
+}
+
+
+function callNumber() {
+    const number = document.getElementById("screen").innerText;
+    if (number === "Digite..." || number.trim() === "") {
+        alert("Digite um número antes de ligar!");
+    } else {
+        alert(`Ligando para ${number}... 📞`);
+    }
+}
